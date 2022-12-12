eba9fe042e301e34095cb28960ec5ce3ea649180
Luiz Victor Andrade <68302376+yLuiz@users.noreply.github.com>
2022-12-12 10:30:41 -0400

    Update flappy.js

--- js/flappy.js
+++ js/flappy.js
@@ -71,7 +71,7 @@ function Passaro(alturaJogo) {
   let voando = false;
 
   this.elemento = novoElemento('img', 'passaro');
-  this.elemento.src = './dom/imgs/passaro.png';
+  this.elemento.src = './imgs/passaro.png';
 
   this.getY = () => parseInt(this.elemento.style.bottom.split('px')[0]);
   this.setY = y => this.elemento.style.bottom = `${y}px`;
